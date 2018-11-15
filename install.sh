a324cf059da1eefa7380ace307c72053fb01dae8
bpweber <bpweber98@gmail.com>
2018-11-14 23:43:30 -0800

    tweaked install script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ cp .vimrc ~/
 cp .wallpaper.jpg ~/Pictures
 sudo cp index.css /usr/share/lightdm-webkit/themes/arch/
 sudo cp main.js /usr/share/lightdm-webkit/themes/arch/
-sudo cp wallpaper.jpg /usr/share/lightdm-webkit/themes/arch/
+sudo cp wallpaper.jpg /usr/share/lightdm-webkit/themes/arch/wallpapers
 sudo mkdir /usr/share/fonts/product-sans
 sudo cp product-sans/* /usr/share/fonts/
 cp solarized.vim ~/.vim/colors/
